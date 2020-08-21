13e8355dae2904da64facfb9d49e48e34aef0447
github username <jhernandez1692@gmail.com>
2020-08-21 17:38:52 -0400

    added new components to all three classes

--- lib/crypto_data/api.rb
+++ lib/crypto_data/api.rb
@@ -23,10 +23,26 @@ class CryptoData::GetCurrency
         # all api data is below in response.body. now being returned by fetch_data
         data = response.body
         ticker = JSON.parse(data)
-        ticker.collect do |tickers|
-            tickers["asset_id"]
-            CryptoData::Currency.new_from_collection(tickers)
-        end
+        # asset_id = ticker["asset_id"]
+        # name = ticker["name"]
+        # type_is_crypto = ticker["type_is_crypto"]
+        # data_start = ticker["data_start"]
+        # data_end = ticker["data_end"]
+        # data_quote_start = ticker["data_quote_start"]
+        # data_quote_end = ticker["data_quote_end"]
+        # data_orderbook_start = ticker["data_orderbook_start"]
+        # data_orderbook_end = ticker["data_orderbook_end"]
+        # data_trade_start = ticker["data_trade_start"]
+        # data_trade_end = ticker["data_trade_end"]
+        # data_symbols_count = ticker["data_symbols_count"]
+        # volume_1hrs_usd = ticker["volume_1hrs_usd"]
+        # volume_1day_usd = ticker["volume_1day_usd"]
+        # volume_1mth_usd = ticker["volume_1mth_usd"]
+        # id_icon = ticker["id_icon"]
+        # binding.pry  
+        CryptoData::Currency.new_from_collection(ticker)  
+          
+            
         
     end
 
--- lib/crypto_data/cli.rb
+++ lib/crypto_data/cli.rb
@@ -39,8 +39,8 @@ class CryptoData::CLI
         puts "Which number currency would you like info about?"
         
 
-        puts "1.  USD"
-        puts "2.  BTC"
+        puts "1.  BTC"
+        puts "2.  USD"
         puts "3.  PLN"
         puts "4.  EUR"
         puts "5.  CNY"
@@ -50,7 +50,7 @@ class CryptoData::CLI
         puts "9.  SEK"
         puts "10. GBP"
 
-        @input = gets.strip.to_i
+        @input = gets.strip.to_i-1
         print_info(@input)
         
     end
@@ -61,26 +61,28 @@ class CryptoData::CLI
         # puts "Which number currency would you like info about?"
         # @input = gets.strip.to_i
 
-        if @input == 1
-            puts "asset_id: USD"
-            puts "name: US Dollar"
-            puts "type_is_crypto: 0"
-            puts "data_start: 2010-07-17"
-            puts "data_end: 2020-08-19"
-            puts "data_quote_start: 2014-02-24T17:43:05.0000000Z"
-            puts "data_quote_end: 2020-08-19T18:06:19.4429175Z"
-            puts "data_orderbook_start: 2014-02-24T17:43:05.0000000Z"
-            puts "data_orderbook_end: 2020-08-05T14:38:00.7082850Z"
-            puts "data_trade_start: 2010-07-17T23:09:17.0000000Z"
-            puts "data_trade_end: 2020-08-19T18:07:35.9831430Z"
-            puts "data_symbols_count: 26527"
-            puts "volume_1hrs_usd: 1118265036950.45"
-            puts "volume_1day_usd: 50408357772429.19"
-            puts "volume_1mth_usd: 1733614557358157.79"
-            puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
+        if @input == 0
+            
+            puts "asset_id: #{CryptoData::Currency.btc.asset_id}"
+            puts "name: #{CryptoData::Currency.btc.name}"
+            puts "type_is_crypto: #{CryptoData::Currency.btc.type_is_crypto}"
+            puts "data_start: #{CryptoData::Currency.btc.data_start}"
+            puts "data_end: #{CryptoData::Currency.btc.data_end}"
+            puts "data_quote_start: #{CryptoData::Currency.btc.data_quote_start}"
+            puts "data_quote_end: #{CryptoData::Currency.btc.data_quote_end}"
+            puts "data_orderbook_start: #{CryptoData::Currency.btc.data_orderbook_start}"
+            puts "data_orderbook_end: #{CryptoData::Currency.btc.data_orderbook_end}"
+            puts "data_trade_start: #{CryptoData::Currency.btc.data_trade_start}"
+            puts "data_trade_end: #{CryptoData::Currency.btc.data_trade_end}"
+            puts "data_symbols_count: #{CryptoData::Currency.btc.data_symbols_count}"
+            puts "volume_1hrs_usd: #{CryptoData::Currency.btc.volume_1hrs_usd}"
+            puts "volume_1day_usd: #{CryptoData::Currency.btc.volume_1day_usd}"
+            puts "volume_1mth_usd: #{CryptoData::Currency.btc.volume_1mth_usd}"
+            puts "id_icon: #{CryptoData::Currency.btc.id_icon}"
+            
             start
 
-        elsif @input == 2
+        elsif @input == 1
 
             puts "asset_id: BTC"
             puts "name: US Dollar"
@@ -100,7 +102,7 @@ class CryptoData::CLI
             puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
             start
 
-        elsif @input == 3
+        elsif @input == 2
 
             puts "asset_id: PLN"
             puts "name: US Dollar"
@@ -120,7 +122,7 @@ class CryptoData::CLI
             puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
             start
 
-        elsif @input == 4
+        elsif @input == 3
 
             puts "asset_id: EUR"
             puts "name: US Dollar"
@@ -140,7 +142,7 @@ class CryptoData::CLI
             puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
             start
 
-        elsif @input == 5
+        elsif @input == 4
 
             puts "asset_id: JPY"
             puts "name: US Dollar"
@@ -160,7 +162,7 @@ class CryptoData::CLI
             puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
             start
 
-        elsif @input == 6
+        elsif @input == 5
 
             puts "asset_id: CNY"
             puts "name: US Dollar"
@@ -180,7 +182,7 @@ class CryptoData::CLI
             puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
             start
 
-        elsif @input == 7
+        elsif @input == 6
 
             puts "asset_id: AUD"
             puts "name: US Dollar"
@@ -200,7 +202,7 @@ class CryptoData::CLI
             puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
             start
 
-        elsif @input == 8
+        elsif @input == 7
 
             puts "asset_id: CHF"
             puts "name: US Dollar"
@@ -220,7 +222,7 @@ class CryptoData::CLI
             puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
             start
 
-        elsif @input == 9
+        elsif @input == 8
 
             puts "asset_id: SEK"
             puts "name: US Dollar"
@@ -240,7 +242,7 @@ class CryptoData::CLI
             puts "id_icon: 0a4185f2-1a03-4a7c-b866-ba7076d8c73b"
             start
 
-        elsif @input == 10
+        elsif @input == 9
 
             puts "asset_id: GBP"
             puts "name: US Dollar"
